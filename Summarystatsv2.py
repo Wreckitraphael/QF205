492d5dfaac7093bc26319c9dee7e81fd567afad4
Raphael <mrloo95@hotmail.com>
2018-10-23 05:12:10 +0800

    Added max Sharpe strategy

--- Summarystatsv2.py
+++ Summarystatsv2.py
@@ -6,6 +6,7 @@ Created on Mon Oct 22 21:01:09 2018
 """
 import pandas as pd
 import numpy as np
+import MatMath as mm
 
 class SummaryStats:
     def __init__(self, data):
@@ -17,7 +18,7 @@ class SummaryStats:
         self.headers=list(data.columns)
         
     def mean(self, output_raw=False):
-        mu=[(sum(self.data.iloc[:,i])/self.data.iloc[:,i].shape[0]) for i in range(self.num_vars)]
+        mu=[sum(self.data.loc[:,i])/self.datapoints[i] for i in self.headers]
         if output_raw==False:
             mu=pd.Series(mu,index=self.headers)
         return mu
@@ -25,36 +26,59 @@ class SummaryStats:
     def var(self, sample=False, output_raw=False):
         mu=self.mean()
         if sample==False:
-            variance=[sum([(self.data.loc[j,i]-mu[i])**2 for j in range(self.datapoints[i])])/self.datapoints[i] for i in self.headers]
+            variance=[sum([(self.data.loc[j,i]-mu[i])**2 
+                     for j in range(self.datapoints[i])])/self.datapoints[i] 
+                     for i in self.headers]
         else:
-            variance=[sum([(self.data.loc[j,i]-mu[i])**2 for j in range(self.datapoints[i])])/(self.datapoints[i]-1) for i in self.headers]
+            variance=[sum([(self.data.loc[j,i]-mu[i])**2 
+                     for j in range(self.datapoints[i])])/(self.datapoints[i]-1) 
+                     for i in self.headers]
         if output_raw==False:
             variance=pd.Series(variance,index=self.headers)
         return variance
     
     def sd(self, sample=False, output_raw=False):
         variances=self.var(sample, output_raw=True)
-        sigma=[i**.5 for i in variances]
+        sigma=[i**0.5 for i in variances]
         if output_raw==False:
-            sigma=pd.Series(variances,index=self.headers)  
+            sigma=pd.Series(sigma,index=self.headers)  
         return sigma
     
     def cov(self, sample=False, output_raw=False):
         mu=self.mean()
         if sample==False:
-            cov_mat=[[sum([(self.data.loc[j,i]-mu[i])*(self.data.loc[j,k]-mu[k]) for j in range(self.datapoints[i])])/self.datapoints[i] for k in self.headers] for i in self.headers]
+            cov_mat=[[sum([(self.data.loc[j,i]-mu[i])*(self.data.loc[j,k]-mu[k]) 
+                    for j in range(self.datapoints[i])])/self.datapoints[i] 
+                    for k in self.headers] 
+                    for i in self.headers]
         else:
-            cov_mat=[[sum([(self.data.loc[j,i]-mu[i])*(self.data.loc[j,k]-mu[k]) for j in range(self.datapoints[i])])/(self.datapoints[i]-1) for k in self.headers] for i in self.headers]
+            cov_mat=[[sum([(self.data.loc[j,i]-mu[i])*(self.data.loc[j,k]-mu[k]) 
+                    for j in range(self.datapoints[i])])/(self.datapoints[i]-1) 
+                    for k in self.headers] 
+                    for i in self.headers]
         if output_raw==True:
             cov_mat=np.array(cov_mat)
         else:
-            cov_mat=pd.DataFrame()
+            cov_mat=pd.DataFrame(cov_mat,index=self.headers,columns=self.headers)
         return cov_mat
 
-prices=pd.read_csv(r"C:\Users\Vincent Loo\Documents\Python Scripts\QF205 Project\QF205\C09.SI.csv")
-test=prices.loc[:,'Open':'Close']    
-test=SummaryStats(test)
-testmean=test.mean()
-#print(testmean['Open'])
-print(test.var())
-print(test.cov())    
+    def cor(self, sample=False, output_raw=False):
+        cov_mat=self.cov(sample, output_raw=True)
+        sigmas=self.sd(sample)
+        sd_mat=np.array([[1/sigmas[i] if i==j else 0.0 for j in self.headers] for i in self.headers])
+        cor_mat=mm.mmult(mm.mmult(sd_mat,cov_mat),sd_mat)
+        if output_raw==False:
+            cor_mat=pd.DataFrame(cor_mat,index=self.headers,columns=self.headers)
+        return cor_mat
+        
+        
+# =============================================================================
+# prices=pd.read_csv('C09.SI.csv')
+# test=prices.loc[:,'Open':'Close']    
+# test=SummaryStats(test)
+# testmean=test.mean()
+# print(test.var())
+# print(test.sd())
+# print(test.cov())
+# print(test.cor())    
+# =============================================================================
