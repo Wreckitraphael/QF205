2cbbfc130490c8328ef290ef92f6c41dc16542bb
Raphael <mrloo95@hotmail.com>
2018-10-26 05:12:49 +0800

    WIP Trading algo

--- Trading.py
+++ Trading.py
@@ -7,16 +7,23 @@ Created on Tue Oct 23 16:30:30 2018
 
 import numpy as np
 import pandas as pd
+from pandas_datareader import iex
 import MatMath as mm
-import Summarystatsv2 as ss
-import MaxSharpe as ms
+#import Summarystatsv2 as ss
+#import MaxSharpe as ms
 
 class Portfolio():
-    def __init__(self, *, stock_data, trade_intervals, datapoints=10, rf):
-        self.data=stock_data
-        self.trade_intervals=trade_intervals
-        self.datapoints=datapoints
-        self.rf=rf
+    def __init__(self, tickers):
+        self.tickers=tickers
+        
+    def get_data(self, tickers=None, start_date=pd.datetime.now().date(), end_date=None, attributes=['open','close']):
+        if tickers==None:
+            tickers=self.tickers
+        if end_date==None:
+            tickers=start_date
+        data=iex.daily.IEXDailyReader(tickers, start_date, end_date)
+        data={i:data.read()[i] for i in attributes}
+        return data
         
     def pos_open(self, start_capital, date, tickers, weights):
         share_price=list(self.data.loc[self.data['Date']==date, tickers].iloc[0,:])
@@ -30,10 +37,15 @@ class Portfolio():
         portfolio_value=sum([i*j for i,j in zip(list(num_shares.values()),share_price)])
         return portfolio_value
     
-    
+class Backtest(Portfolio):
+    def __init__(self, *, capital, trade_dates, )    
 
 
+# =============================================================================
+# all_tickers=['APC', 'AMZN','COP','BMY']
+# portfolio=Portfolio(all_tickers)
+# data=portfolio.get_data(start_date='2017-01-01', end_date='2017-01-31')
+# print(data)
+# =============================================================================
 
 
-
-#def trade(capital, strategy, tickers, **kwargs)
